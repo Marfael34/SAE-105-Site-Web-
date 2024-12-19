0b8ef73a67dfe46140291aa0dcc4ad4fca185858
Marfael34 <raphaelm085@gmail.com>
2024-12-19 15:04:41 +0100

    pulls_moche_noel .html et .js modif

---
+++ JS/pulls_moche_noel.js
@@ -0,0 +1,15 @@
+// liste phrase
+const phrases = [
+   "Le nom de ce chalet est : ",
+    "Les pulls moche de noël",
+  ];
+  
+  let indexPhrase = -1; // Index de la phrase actuelle
+
+  // Fonction pour changer la phrase dans la bulle 
+  function changerPhrase() {
+      // Incrémenter l'index et revenir à zéro si la fin est atteinte
+      indexPhrase = (indexPhrase + 1) % phrases.length;
+      // Changer le texte de la bulle
+      document.getElementById("bulle").innerText = phrases[indexPhrase];
+  }
